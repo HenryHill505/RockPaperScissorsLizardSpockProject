4e1585ab1064e2835d00615774bbcc8549df02b5
HenryHill505 <hhill505@gmail.com>
2018-06-20 16:06:17 -0500

    Added GetPlayerCount() and CreatePlayers() to class GameMaster to create player objects based on users choice for number of players

--- RockPaperScissorsLizardSpock/GameMaster.cs
+++ RockPaperScissorsLizardSpock/GameMaster.cs
@@ -8,9 +8,34 @@ namespace RockPaperScissorsLizardSpock
 {
     public class GameMaster
     {
+        //member variables
+        Player player1;
+        Player player2;
         public GameMaster()
         {
 
+        }
+
+        public string GetPlayerCount()
+        {
+            Console.WriteLine("1 player game or 2 player game?");
+            return Console.ReadLine();
+        }
+
+        public void CreatePlayers()
+        {
+            player1 = new HumanPlayer();
+            switch(GetPlayerCount()){
+                case "1":
+                    player2 = new ComputerPlayer();
+                    break;
+                case "2":
+                    player2 = new HumanPlayer();
+                    break;
+                default:
+                    Console.WriteLine("Error: Entered invalid number of players");
+                    break;
+            }
         }
     }
 }
