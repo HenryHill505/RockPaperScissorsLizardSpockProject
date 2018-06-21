24c4c1d303ab2038206e88cf065743ae999c0d1d
HenryHill505 <hhill505@gmail.com>
2018-06-21 08:05:28 -0500

    Added method ChooseGesture() to class HumanPlayer

--- RockPaperScissorsLizardSpock/Player.cs
+++ RockPaperScissorsLizardSpock/Player.cs
@@ -11,17 +11,38 @@ namespace RockPaperScissorsLizardSpock
         //member variables
         public int name;
         public int winCount;
+        public Gesture chosenGesture;
+        public List<Gesture> gestureList;
 
         //constructor
         public Player()
         {
             winCount = 0;
+            gestureList = new List<Gesture>() { new Rock(), new Lizard(), new Spock(), new Scissors(), new Paper() };
         }
     }
 
     public class HumanPlayer : Player
     {
+        public void ChooseGesture()
+        {
+            Console.WriteLine("Select a gesture: ");
+            int count = 0;
+            foreach (Gesture gesture in gestureList)
+            {
+                count++;
+                Console.Write(count + ". " + gesture.name + "   ");
+            }
+            string inputString = Console.ReadLine();
+            int gestureChoice;
+            
+            while (!int.TryParse(inputString, out gestureChoice) || gestureChoice>gestureList.Count || gestureChoice <= 0)
+            {
+                Console.WriteLine("Invalid input. Enter the number corresponding to your choice");
+            }
 
+            chosenGesture = gestureList[gestureChoice - 1];
+        }
     }
 
     public class ComputerPlayer : Player
